873a8850e34962740de3363ee35e95809d3c822a
divyamarora92 <51014772+divyamarora92@users.noreply.github.com>
2023-12-25 20:42:46 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Menu from "@/components/Menu"
+import ToolBox from "@/components/Toolbox"
+export default function Home() {
+  return (
+    <>
+     <Menu/>
+     <ToolBox/>
+    </>
+   
+
+  )
+}
